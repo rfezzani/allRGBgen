b16ee27d4e94cb80942b05ed463725140cf32824
rfezzani <rfezzani@gmail.com>
2022-02-07 00:24:04 +0100

    Add create_map and apply_map

--- app.py
+++ app.py
@@ -17,15 +17,23 @@ sort_by = {
 }
 
 
-vals = np.arange(256, dtype=np.uint8)
+@st.cache
+def gen_values():
+    print(1)
+    vals = np.arange(256, dtype=np.uint8)
 
-allrgb = skimage.img_as_float(np.array(list(product(vals, vals, vals))))
-_allhsv = color.rgb2hsv(allrgb)
-_alllab = color.rgb2lab(allrgb)
-_allrgbcie = color.rgb2rgbcie(allrgb)
-_allhsvcie = color.rgb2hsv(_allrgbcie)
-_alllabcie = color.rgb2lab(_allrgbcie)
+    allrgb = skimage.img_as_float(np.array(list(product(vals, vals, vals))))
+    _allhsv = color.rgb2hsv(allrgb)
+    _alllab = color.rgb2lab(allrgb)
+    _allrgbcie = color.rgb2rgbcie(allrgb)
+    _allhsvcie = color.rgb2hsv(_allrgbcie)
+    _alllabcie = color.rgb2lab(_allrgbcie)
+    return allrgb, _allhsv, _alllab, _allhsvcie, _alllabcie
 
+
+allrgb, _allhsv, _alllab, _allhsvcie, _alllabcie = gen_values()
+
+print(2)
 with st.sidebar:
     tocie = st.checkbox("Convert to RGB CIE color space")
     px_size = st.selectbox("Super pixel size", [16, 8, 4, 2, 1])
@@ -38,6 +46,7 @@ if render:
     else:
         sort_val = _alllab if sort_fun == "lum" else _allhsv
 
+    print(3)
     nl, nc = 4096, 4096  # Output shape
     res0, res1 = nl // px_size, nc // px_size  # Map shape
 
--- utils.py
+++ utils.py
@@ -24,3 +24,23 @@ def rand_sort(rgb, *args, **kwargs):
     _rgb = rgb.copy()
     np.random.shuffle(_rgb)
     return _rgb
+
+
+def create_map(src):
+    _, inv, count = np.unique(src,
+                              return_inverse=True,
+                              return_counts=True)
+
+    bins = np.prod(src.shape)
+    bags_idx = [x.tolist() for x in np.split(np.arange(bins, dtype=int),
+                                             np.cumsum(count)[:-1])]
+
+    _map = np.array([bags_idx[val].pop() for val in inv]).reshape(src.shape)
+    return _map
+
+
+def apply_map(allrgb, _map, bloc_size):
+    blocs = [b.reshape((bloc_size, bloc_size, 3))
+             for b in np.split(allrgb, bins)]
+    img = np.vstack([np.hstack([blocs[col] for col in row]) for row in idx])
+    return img
