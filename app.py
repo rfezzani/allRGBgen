b7c67b5d38da346c2b1c713d933cb901172a4e26
rfezzani <rfezzani@gmail.com>
2022-02-08 00:12:40 +0100

    Make RGB CIE colorspace default

--- app.py
+++ app.py
@@ -41,32 +41,18 @@ def get_shape(px_size, sort_fun, nl=4096, nc=4096):
 @st.cache
 def gen_values():
     vals = np.arange(256, dtype=np.uint8)
-
     allrgb = np.array(list(product(vals, vals, vals)))
-    _allrgb = skimage.img_as_float(allrgb)
-    _allhsv = color.rgb2hsv(_allrgb)
-    _alllab = color.rgb2lab(_allrgb)
-    return allrgb, _allhsv, _alllab
-
-
-@st.cache
-def gen_cie_values(allrgb):
     _allrgbcie = color.rgb2rgbcie(skimage.img_as_float(allrgb))
     _allhsvcie = color.rgb2hsv(_allrgbcie)
     _alllabcie = color.rgb2lab(_allrgbcie)
-    return _allhsvcie, _alllabcie
+    return allrgb, _allhsvcie, _alllabcie
 
 
 @st.cache
-def get_img(sort_fun, shape, tocie, px_size, nl=4096, nc=4096):
+def get_img(sort_fun, shape, px_size, nl=4096, nc=4096):
 
     allrgb, _allhsv, _alllab = gen_values()
-
-    if tocie:
-        _allhsvcie, _alllabcie = gen_cie_values(allrgb)
-        sort_val = _alllabcie if sort_fun == "lum" else _allhsvcie
-    else:
-        sort_val = _alllab if sort_fun == "lum" else _allhsv
+    sort_val = _alllab if sort_fun == "lum" else _allhsv
 
     allrgb = sort_by[sort_fun](allrgb, sort_val)
 
@@ -87,7 +73,6 @@ def get_png(img):
 
 
 with st.sidebar:
-    tocie = st.checkbox("Convert to RGB CIE color space")
     px_size = st.selectbox("Super pixel size", [32, 16, 8, 4, 2, 1], index=1)
     sort_fun = st.selectbox("Sort strategy", sort_by.keys())
     shape = st.selectbox("Shape", get_shape(px_size, sort_fun).keys())
@@ -95,7 +80,7 @@ with st.sidebar:
 
 cat = get_shape(px_size, sort_fun)["Cat"]
 
-img = get_img(sort_fun, shape, tocie, px_size)
+img = get_img(sort_fun, shape, px_size)
 
 if mozaic:
     img = to_mozaic(img, px_size)
